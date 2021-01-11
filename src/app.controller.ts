81710c1b1afc5f530d93ae2d5e81b19dee80f40b
Emmax <emmax@Emmaxs-MacBook-Pro.local>
2021-01-11 22:01:59 +0100

    feat: 🎸 Sample file upload with nestjs

---
+++ src/app.controller.ts
@@ -0,0 +1,25 @@
+import {
+  Controller,
+  Get,
+  Post,
+  UseInterceptors,
+  UploadedFile,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Post()
+  @UseInterceptors(FileInterceptor('image', { dest: './images' }))
+  async upload(@UploadedFile() file): Promise<File> {
+    return file;
+  }
+}
---
+++ src/main.ts
@@ -0,0 +1,31 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import * as cron from 'node-cron';
+import * as fs from 'fs';
+import * as path from 'path';
+
+async function bootstrap() {
+  cron.schedule('0 0 */1 * * *', () => {
+    console.log('DELETING FILES.......');
+
+    fs.readdir('images', (err, files) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+
+      for (const file of files) {
+        fs.unlink(path.join('images', file), (err) => {
+          if (err) {
+            console.log(err);
+            return;
+          }
+        });
+      }
+    });
+  });
+  const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.PORT || 3000);
+}
+
+bootstrap();
