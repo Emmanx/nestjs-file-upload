abcb6d2b257d113a5cfc1d02fb93ff9e7bf1a92d
Emmax <emmax@Emmaxs-MacBook-Pro.local>
2021-01-13 07:23:32 +0100

    feat: 🎸 Added rate limiting

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import * as cron from 'node-cron';
 import * as fs from 'fs';
 import * as path from 'path';
+import * as rateLimit from 'express-rate-limit';
 
 async function bootstrap() {
   cron.schedule('0 0 */1 * * *', () => {
@@ -24,8 +25,16 @@ async function bootstrap() {
       }
     });
   });
+
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.use(
+    rateLimit({
+      windowMs: 10 * 60 * 1000, // 10 minutes
+      max: 20, // limit each IP to 20 requests per windowMs
+    }),
+  );
+
   await app.listen(process.env.PORT || 3000);
 }
 
